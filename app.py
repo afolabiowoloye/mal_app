2a3bc881331e5fed4dffac3e6e0dc28e3b80f0d5
afolabiowoloye <68096256+afolabiowoloye@users.noreply.github.com>
2024-12-24 21:13:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,10 @@ img_height = 64
 # Streamlit app title
 st.title("Plasmo3Net: Malaria Detection using CNN")
 
-st.write("Testset Confusion Matrix")
+
 image = 'logo/CNN_confusion_matrix_test.png'
 st.image(image, use_column_width=True)
+st.write("Testset Confusion Matrix")
 
 # File uploader
 uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg"])
