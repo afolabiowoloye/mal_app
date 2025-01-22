869a3b1345480ba28c77eeeb866c9a0811c6afee
afolabiowoloye <68096256+afolabiowoloye@users.noreply.github.com>
2025-01-22 23:37:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,29 @@ if selected == "Home":
     </style>
     """, unsafe_allow_html=True)
     
+    st.markdown( """ 
+            <style>
+                /* Target the expander header */
+                div[data-testid="stExpander"] > details > summary {
+                    background-color: #FAEBD7;
+                    color: black;
+                    font-size: 16px;
+                    font-weight: bold;
+                }
+                /* Target the expander content */
+                div[data-testid="stExpander"] > details > div {
+                    background-color: white;
+                    color: blue;
+                    padding: 10px;
+                    border: 1px solid #FAEBD7;
+                }
+            </style>
+            """, 
+            unsafe_allow_html=True )
+    
+    
+    
+    
     with st.expander("**1. Upload Image**", icon=upload_icon):
         st.write("Users can upload microscope slide images containing red blood cells (RBCs). The application supports various image formats for ease of use.")
 
