19e2fcfbede6ce158d21846aa01a8783a165f69a
afolabiowoloye <68096256+afolabiowoloye@users.noreply.github.com>
2025-01-22 16:41:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -104,15 +104,17 @@ if selected == "Home":
     with st.sidebar.header('Instruction on how to use this app'):
           st.sidebar.markdown("""
           <h4 style='color: red;'>How to Use the Web App</h4> 
-          *Step 1: Image Segmentation (For Microscope Slide Images)*
+          **Step 1: Image Segmentation (For Microscope Slide Images)**
+          
           i. If you are working with **microscope slide images**, click the **"Segmentation"** button.
-          ii. The algorithm will automatically segment your images into individual **Red Blood Cell (RBC) images**.
+          ii. The algorithm will automatically segment your images into individual **Red Blood Cell (RBC) images**
+          
           **Step 2: Run Diagnosis (For Segmented RBC Images)**
           i. After segmentation (or if you already have **segmented RBC images**), click the **"Run Diagnosis"** button.
           ii. Upload your segmented RBC images for classification.
           iii. The classification results will be displayed in **real-time**.
           
-          **Note**:
+          **Note**:n\
           If you are working with **pre-segmented RBC images**, skip **Step 1** and go directly to **Step 2** for classification.)
           """, unsafe_allow_html=True)
     
