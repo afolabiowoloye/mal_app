401250b64d3b8a5b7b872066c1a1eb41890008ed
afolabiowoloye <68096256+afolabiowoloye@users.noreply.github.com>
2025-04-27 10:19:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -203,6 +203,8 @@ if selected == "Run Diagnosis":
                     parasitized_count += 1
             
                 # Display the image with prediction
+                st.image(uploaded_file, use_container_width=True)
+                st.markdown(f"""<p style='text-align: center; font-size: 16px;'>Uploaded Image: <strong>{label}</strong></p>""",unsafe_allow_html=True)
                 st.image(uploaded_file, caption=f"Uploaded Image: {label}", use_container_width=True)
             
                 # Update progress
